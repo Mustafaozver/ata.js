87fb16469908dc608395e7c42783377b1dcdd5e2
Mustafa ÖZVER <mustafa.ozver@hotmail.com>
2024-09-13 22:43:11 +0300

    13e2024-2

--- bin/run.js
+++ bin/run.js
@@ -4,7 +4,7 @@
 	const CWD = ATA.CWD;
 	const MWD = ATA.MWD;
 	console.log(process.argv);
-	ATA.Inject(ATA.Path.join(ATA.CWD, "./App/Main.JS"), {
+	ATA.Inject("./App/Main.JS", {
 		ATA,
 	}, [...process.argv.slice(2)]);
 })(require("../index.js")());
